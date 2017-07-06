967977404ad4f2c1a975db393232b56d88f460d2
kctech <kctech@kctech-PC>
2017-07-06 15:12:30 +0530

    opgfg

---
+++ Object Repository/ty.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ty</name>
+   <tag></tag>
+   <elementGuidId>8977e3c2-d5f5-4dcc-84a9-bf8b777b6bc0</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>fff</value>
+   </webElementProperties>
+</WebElementEntity>
